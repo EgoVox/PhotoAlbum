90077016a29b755b2b6ff2aa57e03d94055aff8e
stephane <stephane@wivzem.fr>
2024-08-19 10:20:37 +0200

    add views, edit_inline and carousel

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,6 +1,10 @@
 class PhotoController < ApplicationController
+  # Avant d'exécuter les actions create et destroy, on exécute la méthode set_album
+  before_action :set_album
 
+  # l'action create est responsable de la création d'une nouvelle photo en utilisant les paramètres du formulaire
   def create
+    # On crée une nouvelle instance de photo pour l'album. On préfère un .build à un .new pour que l'association au bon album soit faite via album.photos
     @photo = @album.photos.build(photo_params)
     if @photo.save
       redirect_to @photo.album, notice: 'Photo was successfully created.'
@@ -9,18 +13,22 @@ class PhotoController < ApplicationController
     end
   end
 
+  # l'action destroy est responsable de la suppression d'une photo
   def destroy
     @photo = @album.photos.find(params[:id])
     @photo.destroy
     redirect_to @photo.album, notice: 'Photo was successfully destroyed.'
   end
 
+  # On définit les méthodes privées de la classe
   private
 
+  #  On définit la méthode set_album qui récupère l'album à partir de l'id passé dans les paramètres
   def set_album
     @album = Album.find(params[:album_id])
   end
 
+  # On définit les paramètres autorisés pour la création d'une photo
   def photo_params
     params.require(:photo).permit(:image, :description)
   end
