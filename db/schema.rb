2d65d7f3ad18e0112263c3ef8d0cfd782d13ffb3
stephane <stephane@wivzem.fr>
2024-08-20 15:19:29 +0200

    Add slug to have link with album name instead of number

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_19_082516) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_20_125047) do
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_albums_on_slug", unique: true
   end
 
   create_table "photos", force: :cascade do |t|
